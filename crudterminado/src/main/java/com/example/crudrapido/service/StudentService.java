9fa75472dbeaebf0a477c46cf4ae5d1973598bdc
DanielRealpe <159152088+DanielRealpe@users.noreply.github.com>
2024-03-19 23:29:02 -0500

    Add files via upload

---
+++ crudterminado/src/main/java/com/example/crudrapido/service/StudentService.java
@@ -0,0 +1,38 @@
+package com.example.crudrapido.service;
+
+import java.util.Optional;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.crudrapido.entity.Student;
+import com.example.crudrapido.repository.StudentRepository;
+
+@Service
+
+public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student getStudent(Long id) {
+        return studentRepository.findById(id)
+                                .orElse(null);
+    }
+    
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void saveOrUpdate(Student student) {
+        studentRepository.save(student);
+    }
+
+    public void delete(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+}
